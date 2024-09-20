aad2668ff219301c297d25f51a6536ed89ef95ee
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-09-20 17:47:17 +0200

    Création d'un nouveau projet versionné

--- Exo2/src/exo2/Exo2.java
+++ Exo2/src/exo2/Exo2.java
@@ -1,6 +1,7 @@
 // RAPHAEL VACHERET EXO2 20/09
- */
 package exo2;
+
+import java.util.Scanner;
 
 /*
  *
@@ -18,10 +19,14 @@ public class Exo2 {
         int ind; //indice
         nb=5;
         result=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n Entrer le nombre :");
+        nb=sc.nextInt(); // On demande a sc de donner le prochain entier
 // Addition des nb premiers entiers
         ind=1;
         while (ind <= nb) {
         result=result+ind;
+        ind=ind+1;
         }
         // Affichage du resultat
         System.out.println();
