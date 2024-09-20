c5b98c34d5cb64f9597a18c4193fc652c12a9349
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-09-20 17:33:01 +0200

    Création d'un nouveau projet versionné

--- Exo2/src/exo2/Exo2.java
+++ Exo2/src/exo2/Exo2.java
@@ -1,10 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+// RAPHAEL VACHERET EXO2 20/09
  */
 package exo2;
 
-/**
+/*
  *
  * @author Administrateur
  */
@@ -14,7 +12,20 @@ public class Exo2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        //Declaration des variables
+        int nb; // nombre dentiers a additionner
+        int result; // resultat
+        int ind; //indice
+        nb=5;
+        result=0;
+// Addition des nb premiers entiers
+        ind=1;
+        while (ind <= nb) {
+        result=result+ind;
+        }
+        // Affichage du resultat
+        System.out.println();
+        System.out.println("La somme des "+ nb + "entiers est: "+result);
     }
     
 }
