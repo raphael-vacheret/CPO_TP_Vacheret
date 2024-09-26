365cb397e0dfcc238a096eb6f36df9d6172a958a
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-09-26 16:58:12 +0200

    tp1 avncement

--- TP1_guessMyNumber_VACHERET/src/tp1_guessmynumber_vacheret/TP1_guessMyNumber_VACHERET.java
+++ TP1_guessMyNumber_VACHERET/src/tp1_guessmynumber_vacheret/TP1_guessMyNumber_VACHERET.java
@@ -16,27 +16,82 @@ public class TP1_guessMyNumber_VACHERET {
     public static void main(String[] args) {
         // TODO code application logic here
         Scanner sc = new Scanner(System.in);
+        int calcul;
         int valeur;
         int i=0;
         Random generateurAleat = new Random();
-        int n = generateurAleat.nextInt(100);
-        while (i<100) {
-            System.out.println("\n entrer une valeur :");
-            valeur=sc.nextInt();
-            if (valeur==n) {
-                System.out.println("\n gagné");
-                System.out.println("vous avez utiliser :"+i+" coups");
-                break;
+        System.out.println("choissise un mode de difficulté" );
+        System.out.println("1) mode facile" );
+        System.out.println("2) mode normal" );
+        System.out.println("3) mode difficile" );
+        calcul=sc.nextInt();
+        if (calcul==1) {
+            int n = generateurAleat.nextInt(50);
+            while (i<100) {
+                System.out.println("\n entrer une valeur :");
+                valeur=sc.nextInt();
+                if (valeur==n) {
+                    System.out.println("\n gagné");
+                    System.out.println("vous avez utiliser :"+i+" coups");
+                    break;
+                }
+                else if (valeur<n-10) {
+                    System.out.println("\n vraiment trop petit");
+                    i+=1;
+                }
+                else if (valeur>n+10) {
+                    System.out.println("\n vraiment trop grand");
+                    i+=1;
+                }
+                else if (valeur<n) {
+                    System.out.println("\n trop petit");
+                    i+=1;
+                }
+                else if (valeur>n) {
+                    System.out.println("\n tros grand");
+                    i+=1;
+                }
             }
-            else if (valeur<n) {
-                System.out.println("\n trop petit");
-                i+=1;
+        }
+        else if (calcul==2) {
+            int n = generateurAleat.nextInt(100);
+            while (i<20) {
+                System.out.println("\n entrer une valeur :");
+                valeur=sc.nextInt();
+                if (valeur==n) {
+                    System.out.println("\n gagné");
+                    System.out.println("vous avez utiliser :"+i+" coups");
+                    break;
+                }
+                else if (valeur<n) {
+                    System.out.println("\n trop petit");
+                    i+=1;
+                }
+                else if (valeur>n) {
+                    System.out.println("\n tros grand");
+                    i+=1;
+                }
             }
-            else if (valeur>n) {
-                System.out.println("\n tros grand");
-                i+=1;
+        }
+        else if (calcul==3) {
+            int n = generateurAleat.nextInt(500);
+            while (i<11) {
+                System.out.println("\n entrer une valeur :");
+                valeur=sc.nextInt();
+                if (valeur==n) {
+                    System.out.println("\n gagné");
+                    System.out.println("vous avez utiliser :"+i+" coups");
+                    break;
+                }
+                else if (valeur<n) {
+                    System.out.println("\n trop petit");
+                    i+=1;
+                }
+                else if (valeur>n) {
+                    System.out.println("\n tros grand");
+                    i+=1;
+                }
             }
         }
     }
-    
 }
