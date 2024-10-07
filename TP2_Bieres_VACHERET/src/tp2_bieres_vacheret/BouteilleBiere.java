4a1e87ffa36eac29ab44b6cb05f1b9aad27db546
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-10-07 17:10:55 +0200

    tp1 avncement

---
+++ TP2_Bieres_VACHERET/src/tp2_bieres_vacheret/BouteilleBiere.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp2_bieres_vacheret;
+
+/**
+ *
+ * @author Administrateur
+ */
+public class BouteilleBiere {
+    String nom;
+    double degreAlcool;
+    String brasserie;
+    boolean ouverte;
+    public void lireEtiquette() {
+        System.out.println("Bouteille de " + nom +" (" + degreAlcool +" degres) \nBrasserie : " + brasserie ) ;
+    }
+    public BouteilleBiere(String unNom, double unDegre, String uneBrasserie) {
+    nom = unNom;
+    degreAlcool = unDegre;
+    brasserie = uneBrasserie;
+    ouverte = false;
+    }
+    public boolean Décapsuler() {
+        boolean test;
+        if (ouverte==false) {
+            test=true;
+        }
+        else {
+            
+            test=false;
+        }
+        return test;
+    }
+
+
+}
