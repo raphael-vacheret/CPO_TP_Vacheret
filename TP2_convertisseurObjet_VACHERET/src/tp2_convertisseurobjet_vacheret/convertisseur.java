18bfe5e134726866fe7230c9e48598454b68702f
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-10-11 08:43:28 +0200

    tp2 avncement

--- TP2_convertisseurObjet_VACHERET/src/tp2_convertisseurobjet_vacheret/convertisseur.java
+++ TP2_convertisseurObjet_VACHERET/src/tp2_convertisseurobjet_vacheret/convertisseur.java
@@ -10,49 +10,49 @@ package tp2_convertisseurobjet_vacheret;
  */
 public class convertisseur {
     int nbConversions;
-    public Convertisseur () {
+    public convertisseur () {
         nbConversions = 0 ;
     }
 
     public double CelciusVersKelvin (double valeur) {
         double tKelvin;
         tKelvin=valeur+273;
-        return tKelvin;
         nbConversions += 1 ;
+        return tKelvin;
     }
     public double KelvinVersCelcius (double valeur) {
         double Celcius;
         Celcius=valeur-273;
-        return Celcius;
         nbConversions += 1 ;
+        return Celcius;
     }
     public double FarenheitVersCelcius (double valeur) {
         double Celcius;
         Celcius=(valeur-32)*5/9;
-        return Celcius;
         nbConversions += 1 ;
+        return Celcius;
     }
     public double CelciusVersFarenheit (double valeur) {
         double Frn;
         Frn=(valeur*9/5)+32;
-        return Frn;
         nbConversions += 1 ;
+        return Frn;
     }
     public double KelvinVersFarenheit (double valeur) {
         double Frn;
         Frn=((valeur-32)*5/9)+273;
-        return Frn;
         nbConversions += 1 ;
+        return Frn;
     }
     public double FarenheitVersKelvin (double valeur) {
         double Kelvin;
         Kelvin=((valeur-273)*9/5)+32;
-        return Kelvin;
         nbConversions += 1 ;
+        return Kelvin;
     }
     @Override
     public String toString () {
-        return "nb de conversions"+ nbConversions;
+        return "nb de conversions" + nbConversions;
     }
 
     
