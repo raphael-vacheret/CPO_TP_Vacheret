14274fb4edcecdd813e276005b0a872fe1a3d583
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-10-11 19:08:18 +0200

    tp2 avncement

--- TP2_manip_VACHERET/src/tp2_manip_vacheret/TP2_manip_VACHERET.java
+++ TP2_manip_VACHERET/src/tp2_manip_vacheret/TP2_manip_VACHERET.java
@@ -15,27 +15,30 @@ public class TP2_manip_VACHERET {
         assiette3.nbCalories) ;
         Moussaka assiette40 = new Moussaka(100);
         Moussaka assiette50 = assiette40;
-        Moussaka assiette41 = new Moussaka(100);
+        Moussaka assiette41 = new Moussaka(200);
         Moussaka assiette51 = assiette41;
-        Moussaka assiette42 = new Moussaka(100);
+        Moussaka assiette42 = new Moussaka(300);
         Moussaka assiette52 = assiette42;
-        Moussaka assiette43 = new Moussaka(100);
+        Moussaka assiette43 = new Moussaka(400);
         Moussaka assiette53 = assiette43;
-        Moussaka assiette44 = new Moussaka(100);
+        Moussaka assiette44 = new Moussaka(500);
         Moussaka assiette54 = assiette44;
-        Moussaka assiette45 = new Moussaka(100);
+        Moussaka assiette45 = new Moussaka(600);
         Moussaka assiette55 = assiette45;
-        Moussaka assiette46 = new Moussaka(100);
+        Moussaka assiette46 = new Moussaka(700);
         Moussaka assiette56 = assiette46;
-        Moussaka assiette47 = new Moussaka(100);
+        Moussaka assiette47 = new Moussaka(800);
         Moussaka assiette57 = assiette47;
-        Moussaka assiette48 = new Moussaka(100);
+        Moussaka assiette48 = new Moussaka(900);
         Moussaka assiette58 = assiette48;
-        Moussaka assiette49 = new Moussaka(100);
+        Moussaka assiette49 = new Moussaka(1000);
         Moussaka assiette59 = assiette49;
         
+        Moussaka[] tab = new Moussaka[10];
         
-
+        for (int i = 0; i < tab.length; i++) {
+            tab[i] = new Moussaka();
+        }
     }
     
 }
