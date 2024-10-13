b246be187eb2f922a32553821f46299fc6432aeb
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-10-13 12:43:03 +0200

    tp2 avncement

---
+++ TP2_relation_1_VACHERET/src/tp2_relation_1_vacheret/TP2_relation_1_VACHERET.java
@@ -0,0 +1,14 @@
+// VACHERET RAPHHAEL 13/10 TP2 EXO 4
+package tp2_relation_1_vacheret;
+public class TP2_relation_1_VACHERET {
+    public static void main(String[] args) {
+        Voiture uneClio = new Voiture ("Clio", "Renault", 5 ) ;
+        Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ;
+        Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ;
+        Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
+        Personne bob = new Personne("Bobby", "Sixkiller");
+        Personne reno = new Personne("Reno", "Raines");
+        System.out.println("liste des voitures disponibles "+ uneClio +"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+    }
+    
+}
