d38c17345062d9ccb18049268dc53dbb1ef4fcd6
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-10-15 10:42:17 +0200

    tp2 avncement

--- TP2_relation_1_VACHERET/src/tp2_relation_1_vacheret/TP2_relation_1_VACHERET.java
+++ TP2_relation_1_VACHERET/src/tp2_relation_1_vacheret/TP2_relation_1_VACHERET.java
@@ -6,9 +6,30 @@ public class TP2_relation_1_VACHERET {
         Voiture uneAutreClio = new Voiture ("Clio", "Renault", 5 ) ;
         Voiture une2008 = new Voiture ("2008", "Peugeot", 6 ) ;
         Voiture uneMicra = new Voiture ("Micra", "Nissan", 4 ) ;
+        Voiture unepouma = new Voiture ("pouma", "coupra", 10 ) ;
         Personne bob = new Personne("Bobby", "Sixkiller");
         Personne reno = new Personne("Reno", "Raines");
         System.out.println("liste des voitures disponibles "+ uneClio +"\n" + uneAutreClio + "\n" + une2008 + "\n" + uneMicra ) ;
+        bob.liste_voitures[0] = uneClio ;
+        bob.nbvoitures = bob.nbvoitures++ ;
+        uneClio.Proprietaire = bob ;
+        System.out.println("la premiere voiture de Bob est " +bob.liste_voitures[0] ) ;
+        bob.liste_voitures[1] = une2008 ;
+        bob.nbvoitures = bob.nbvoitures++ ;
+        une2008.Proprietaire = bob ;
+        System.out.println("la deuxieme voiture de Bob est " +bob.liste_voitures[1] ) ;
+        reno.liste_voitures[0] = uneAutreClio ;
+        reno.nbvoitures = reno.nbvoitures ;
+        uneAutreClio.Proprietaire = reno ;
+        System.out.println("la premiere voiture de Bob est " +reno.liste_voitures[0] ) ;
+        reno.liste_voitures[1] = uneMicra ;
+        reno.nbvoitures = reno.nbvoitures ;
+        uneMicra.Proprietaire = reno ;
+        System.out.println("la deuxieme voiture de Bob est " +reno.liste_voitures[1] ) ;
+        reno.ajouter_voiture(unepouma);
+        bob.ajouter_voiture(unepouma);
+        System.out.println("la troisieme voiture de Bob est " +reno.liste_voitures[2] ) ;
+        System.out.println("la troisieme voiture de Bob est " +bob.liste_voitures[2] ) ;
     }
     
 }
