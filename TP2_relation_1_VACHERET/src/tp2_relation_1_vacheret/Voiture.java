9f5feacd7ea7ce8326583deffd4d9230a09f2370
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-10-15 10:45:23 +0200

    tp2 avncement

--- TP2_relation_1_VACHERET/src/tp2_relation_1_vacheret/Voiture.java
+++ TP2_relation_1_VACHERET/src/tp2_relation_1_vacheret/Voiture.java
@@ -4,16 +4,17 @@ public class Voiture {
     String modele;
     String marque ;
     int puissanceCV;
+    Personne Proprietaire=null;
+    Voiture liste_voitures;
     public Voiture(String Unmodele, String Unemarque, int Unepuissance) {
         modele=Unmodele;
         marque= Unemarque;
         puissanceCV= Unepuissance;
-        Voiture liste_voitures;
     }
     @Override
     public String toString() {
         String chaine_a_retourner;
-        chaine_a_retourner = "la voiture est une "+modele+" "+marque+" avec une puissance de "+puissanceCV+"";
+        chaine_a_retourner = "une "+modele+" "+marque+" avec une puissance de "+puissanceCV+"";
         return chaine_a_retourner ;
     }
 }
