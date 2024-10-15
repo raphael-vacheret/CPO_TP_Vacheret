31ab8abe1db93bf9a786295f8fccb0df9f818fdf
Raphael Vacheret <raphael.vacheret@epfedu.fr>
2024-10-15 10:42:49 +0200

    tp2 avncement

--- TP2_relation_1_VACHERET/src/tp2_relation_1_vacheret/Personne.java
+++ TP2_relation_1_VACHERET/src/tp2_relation_1_vacheret/Personne.java
@@ -3,11 +3,24 @@ package tp2_relation_1_vacheret;
 public class Personne {
     String nom;
     String prenom;
-    int nbvoitures;
-    Personne proprietaire;
+    int nbvoitures=0;
+    Voiture [] liste_voitures = new Voiture [3] ;
+    
     public Personne(String unNom, String Unprenom) {
         nom = unNom;
         prenom = Unprenom;
+    }
+    public boolean ajouter_voiture (Voiture voiture_a_ajouter) {
+        if (voiture_a_ajouter.Proprietaire != null) {
+            return false;
+        }
+         if (nbvoitures >= 3) {
+            return false;
+        }
+        liste_voitures[nbvoitures] = voiture_a_ajouter;
+        nbvoitures++;
+        voiture_a_ajouter.Proprietaire = this ; 
+        return true;
     }
     @Override
     public String toString() {
